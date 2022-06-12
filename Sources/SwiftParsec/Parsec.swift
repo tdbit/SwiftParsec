b221688181e878b39fc4eaf2769b110ad671a08a
Tom Drummond <tom@updtd.com>
2022-06-12 09:09:11 -0700

    Move Stream protocol to Stream.swift

--- Sources/SwiftParsec/Parsec.swift
+++ Sources/SwiftParsec/Parsec.swift
@@ -363,37 +363,3 @@ public extension Parsec where UserState == () {
         }
     }
 }
-
-// ==============================================================================
-// Extension containing useful methods to run a parser.
-/// A `Stream` instance is responsible for maintaining the position of the
-/// parser's stream.
-public protocol Stream: Collection, ExpressibleByArrayLiteral
-where ArrayLiteralElement == Element {}
-
-extension String: Stream {
-    /// Create an instance containing `elements`.
-    public init(arrayLiteral elements: String.Iterator.Element...) {
-        self.init(elements)
-    }
-}
-
-// ==============================================================================
-/// Types conforming to the `EmptyInitializable` protocol provide an empty
-/// intializer.
-public protocol EmptyInitializable {
-    init()
-}
-
-// ==============================================================================
-// Extensions implementing the `Stream` protocol for various collections.
-
-extension Array: Stream, EmptyInitializable {}
-
-extension ContiguousArray: Stream, EmptyInitializable {}
-
-extension ArraySlice: Stream, EmptyInitializable {}
-
-extension Dictionary: EmptyInitializable {}
-
-extension Set: EmptyInitializable {}
